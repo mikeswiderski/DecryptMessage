db4c113c9240dfeaae587eb0c17c8a4446a8e98f
Michal Swiderski <contact@michalswiderski.com>
2021-04-13 11:15:42 -0400

    decryptMessage v.1

--- decryptMessage/wsgi.py
+++ decryptMessage/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DecryptMessage project.
+WSGI config for decryptMessage project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DecryptMessage.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decryptMessage.settings')
 
 application = get_wsgi_application()
